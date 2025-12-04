5bbbe740c85ce15714097266f26205773988a33e
Mishal Ramzan <mishalramzan64@gmail.com>
2025-12-04 21:45:40 +0500

    day 4: js basics

---
+++ day4.js
@@ -0,0 +1,21 @@
+let name = "Mishal";
+let age = 25;
+let city = "Multan";
+console.log(name, age, city);
+
+let a =5;
+let b=9;
+console.log("sum = ", a+b);
+console.log("product: ", a*b);
+
+if (age >=18){
+    console.log("Eligible");
+}
+else{
+    console.log("not Eligible");
+}
+
+function welcome(){
+    console.log("welcome to day4 of my learning javascript")
+}
+welcome();
